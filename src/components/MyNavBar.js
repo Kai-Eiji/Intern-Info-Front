f29715445fc49d2da304b240562aafc21475aff0
Kai-Eiji <kaiburkholder.jp@gmail.com>
2021-06-25 14:16:38 -0700

    make navbar collapse

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -7,14 +7,17 @@ import React, { Component } from "react";
 class MyNavBar extends Component{
     render(){
         return(
-            <Navbar bg="primary" variant="dark">
+            <Navbar bg="primary" variant="dark" expand="lg">
                 <Navbar.Brand href="/">Tech Intern Info</Navbar.Brand>
-                <Nav className="mr-auto">
-                  <Nav.Link href="/">Home</Nav.Link>
-                  <Nav.Link href="/search">Search</Nav.Link>
-                  <Nav.Link href="/cpdata">Company</Nav.Link>
-                  <Nav.Link href="/ranking">Ranking</Nav.Link>
-                </Nav>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/search">Search</Nav.Link>
+                    <Nav.Link href="/cpdata">Company</Nav.Link>
+                    <Nav.Link href="/ranking">Ranking</Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
               </Navbar>
         );
     }
